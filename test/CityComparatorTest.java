5d09f8c10a8ef194418993675e39a283e6ee3a03
Viraj Shah <vr510744@dal.ca>
2021-06-20 02:28:19 +0530

    All 3 test methods of CityComparator class is created

--- test/CityComparatorTest.java
+++ test/CityComparatorTest.java
@@ -10,22 +10,33 @@ class CityComparatorTest {
   public static final int cityDistanceShort = 1;
   public static final int cityDistanceLong = 2;
 
+  CityComparator c = new CityComparator();
   /* 1a. compare returns negative if 0 <= x.distance <= y.distance */
   @Test
-  void compare_xSmaller() {
+  public void compare_xSmaller() {
 
+    City c1 = new City(cityDistanceShort);
+    City c2 = new City(cityDistanceLong);
+
+    assertTrue(c.compare(c1,c2) > 0,"city distance is long so false");
   }
 
   /* 1b. compare returns 0 if 0 <= x.distance == y.distance */
   @Test
   void compare_xEqual() {
 
+    City c1 = new City(cityDistanceShort);
+    City c2 = new City(cityDistanceShort);
+    assertTrue(c.compare(c1,c2) == 0,"city distance is long so false");
   }
 
   /* 1c. compare returns positive if 0 <= x.distance <= y.distance */
   @Test
   void compare_xLarger() {
 
+    City c1 = new City(cityDistanceLong);
+    City c2 = new City(cityDistanceShort);
+    assertTrue(c.compare(c1,c2) < 0,"city distance is long so false");
   }
 
 }
