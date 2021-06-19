dc0090257e9449f8b965fe0faf22ab4ca4c8fdf3
Viraj Shah <vr510744@dal.ca>
2021-06-20 02:46:26 +0530

    All test method for getLink() method is created in CityTest class

--- test/CityTest.java
+++ test/CityTest.java
@@ -1,4 +1,8 @@
 import  org.junit.jupiter.api.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CityTest {
@@ -84,6 +88,48 @@ public class CityTest {
         City c1 = new City(3);
         City c2 = new City(3);
         assertTrue(c.compare(c1,c2) == 0,"False");
+    }
+
+    @Test
+    public void testGetLinksTo_true()
+    {
+        City c1 = new City("Toronto");
+        City c2 = new City("Halifax");
+        Set<Link> hs = new HashSet<Link>();
+        Link l = new Link(c1,c2,3);
+        assertTrue(c1.getLinksTo(c2,hs)==true,"False");
+    }
+
+    @Test
+    public void testGetLinksTo_false_condition1()
+    {
+        City c1 = new City("Toronto");
+        City c2 = new City("Halifax");
+        Set<Link> hs = new HashSet<Link>();
+        //Link l = new Link(c1,c2,3);
+        assertTrue(c1.getLinksTo(c2,hs)==false,"False");
+    }
+    @Test
+    public void testGetLinksTo_false_condition2()
+    {
+        City c1 = new City("Toronto");
+        City c2 = new City("Halifax");
+        Set<Link> hs = new HashSet<Link>();
+        Link l = new Link(c1,c2,3);
+        l.setUsed(false);
+        assertTrue(c1.getLinksTo(c2,hs)==false,"False");
+    }
+    @Test
+    public void testGetLinksTo_false_condition3()
+    {
+        City c1 = new City("Toronto");
+        City c2 = new City("Halifax");
+        City c3 = new City("Halifax");
+        Set<Link> hs = new HashSet<Link>();
+        Link l = new Link(c1,c3,3);
+        Link l1 = new Link(c1,c2,3);
+        c3.parent = l;
+        assertTrue(c3.getLinksTo(c1,hs)==false,"False");
     }
 
 }
